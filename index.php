9cfcd146c0cb730efedf5a99c9a1aedff2555e40
10daisuke64 <daisukesumida64@gmail.com>
2021-12-09 23:36:06 +0900

    first_submission

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
 <?php
+if(!empty($_POST)){
+  date_default_timezone_set('Asia/Tokyo');
+  $post_time = date('Y/m/d');
+  $writeArray = [$post_time,$_POST["shop"],$_POST["price"]];
 
+  var_dump($writeArray);
 
+  $file = fopen('./csv/data_utf8.csv', 'a');
+  flock($file, LOCK_EX);
+  fputcsv($file, $writeArray);
+  flock($file, LOCK_UN);
+  fclose($file);
 
+  // 二重送信対策
+  header('Location:index.php');
+  exit();
+}
+
+$dataArray = [];
+$file = fopen('./csv/data_utf8.csv', 'r');
+flock($file, LOCK_EX);
+if ($file) {
+  while ($line = fgetcsv($file)) {
+    // 空行の判定
+    if( implode($line) != null && !empty($line) ){
+      // 時間文字列をsecondsへ変換
+      $line[0] = strtotime($line[0]);
+      $dataArray[] = $line;
+    }
+  }
+}
+flock($file, LOCK_UN);
+fclose($file);
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -34,48 +62,46 @@
 
       <section class="section">
         <div class="wrapper">
-          <h2 class="section-title">入力フォーム</h2>
+          <h2 class="section-title">針葉樹合板の価格を登録</h2>
+
           <form class="form" action="" method="post">
-            <dl>
-              <dt>資材名</dt>
-              <dd>
-                <select name="material">
-                  <option disabled selected>選択してください</option>
-                  <option value="shiyoju">針葉樹合板</option>
-                  <option value="plaster">石膏ボード</option>
-                  <option value="kaku3">45角3m</option>
-                  <option value="kaku4">45角4m</option>
-                </select>
-              </dd>
-            </dl>
-            <dl>
-              <dt>価格</dt>
-              <dd class="short">
-                <input type="number" name="price" min="0" max="5000"><span>円</span>
-              </dd>
-            </dl>
-            <dl>
-              <dt>店名</dt>
-              <dd>
-                <select name="shop">
-                  <option disabled selected>選択してください</option>
-                  <option value="nafco">ナフコ</option>
-                  <option value="juntendo">ジュンテンドー</option>
-                  <option value="nohgata">直方建材</option>
-                  <option value="other">その他</option>
-                </select>
-              </dd>
-            </dl>
+            <div class="radio">
+              <label>
+                <input type="radio" name="shop" value="nafco">
+                <img src="./img/nafco_logo.svg" width="24" height="24">
+              </label>
+              <label>
+                <input type="radio" name="shop" value="juntendo">
+                <img src="./img/juntendo.png" width="22" height="22">
+              </label>
+            </div>
+            <div class="price">
+              <input type="number" name="price" min="0" max="5000"><span>円</span>
+            </div>
+            <input class="submit" type="submit" value="登録">
           </form>
+
         </div>
       </section>
 
+      <!-- <section class="section">
+        <div class="wrapper">
+          <h2 class="section-title">過去の記録</h2>
+          <div class="result-past">
+            <div class="result-past__item result-max" id="js-result-max"></div>
+            <div class="result-past__item result-min" id="js-result-min"></div>
+            <div class="result-past__item result-recent" id="js-result-recent"></div>
+          </div>
+        </div>
+      </section> -->
+
       <section class="section">
         <div class="wrapper">
-          <h2 class="section-title">価格変動</h2>
-          <div class="result-price" id="js-result-price"></div>
+          <h2 class="section-title">針葉樹合板の価格変動</h2>
+          <canvas class="result-price" id="js-result-price" height="350"></canvas>
         </div>
       </section>
+
     </main>
     <!-- //main -->
 
@@ -87,7 +113,141 @@
 
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="./js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.6.0/dist/chart.min.js"></script>
+
+    <script type="text/javascript">
+
+      // jsonへ変換
+      const dataArray = <?=json_encode($dataArray)?>;
+      // 時系列に変換
+      dataArray.sort(function(first, second){
+        if ( first[0] > second[0] ){
+          return 1;
+        } else if ( first[0] < second[0] ){
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      console.log(dataArray);
+
+      // 時間表示の変換
+      function convertTimestampToDate(timestamp) {
+        const _d = timestamp ? new Date(timestamp * 1000) : new Date();
+        const Y = _d.getFullYear();
+        const m = (_d.getMonth() + 1).toString().padStart(1, '0');
+        const d = _d.getDate().toString().padStart(1, '0');
+        const H = _d.getHours().toString().padStart(2, '0');
+        const i = _d.getMinutes().toString().padStart(2, '0');
+        const s = _d.getSeconds().toString().padStart(2, '0');
+        return `${m}/${d}`;
+      }
+
+      // データ配列へ変換
+      const nafcoArray = [];
+      const jutendoArray = [];
+
+      dataArray.forEach(function (data) {
+        if ( data[1] == "nafco" ) {
+          let nafco_inner_array = {
+            x: convertTimestampToDate(data[0]),
+            y: Number(data[2])
+          };
+          nafcoArray.push(nafco_inner_array);
+        } else if ( data[1] == "juntendo" ) {
+          let jutendo_inner_array = {
+            x: convertTimestampToDate(data[0]),
+            y: Number(data[2])
+          };
+          jutendoArray.push(jutendo_inner_array);
+        }
+      });
+
+      // グラフのデータ
+      const chart_data = {
+        datasets: [
+          {
+            label: 'ナフコ',
+            data: nafcoArray,
+            borderColor: '#4293f7',
+            backgroundColor: '#90bff9',
+            yAxisID: 'y',
+            tension: 0.3,
+            fill: false,
+          },
+          {
+            label: 'ジュンテンドー',
+            data: jutendoArray,
+            borderColor: '#fc4d4d',
+            backgroundColor: '#ff8585',
+            yAxisID: 'y',
+            tension: 0.3,
+            fill: false,
+          }
+        ]
+      };
+
+      // グラフのオプション
+      const config = {
+        type: 'line',
+        data: chart_data,
+        options: {
+          responsive: true,
+          interaction: {
+            mode: 'index',
+            intersect: false
+          },
+          plugins: {
+            legend: {
+              labels: {
+                font: {
+                  size: 10,
+                  family: "'Noto Sans JP', sans-serif",
+                  weight: '700'
+                }
+              }
+            }
+          },
+          stacked: false,
+          scales: {
+            xAxis: {
+              grid: {
+                borderColor: '#777777',
+              },
+              ticks: {
+                font: {
+                  size: 10,
+                  family: "'Noto Sans JP', sans-serif",
+                }
+              }
+            },
+            y: {
+              type: 'linear',
+              display: true,
+              position: 'left',
+              suggestedMin: 600,
+              suggestedMax: 2000,
+              grid: {
+                borderColor: '#777777',
+              },
+              ticks: {
+                font: {
+                  size: 10,
+                  family: "'Noto Sans JP', sans-serif",
+                },
+                callback: function(value){
+      		        return  value + '円'
+      	        }
+              },
+            }
+          }
+        }
+      };
+      const myChart = new Chart(
+        document.getElementById('js-result-price'),
+        config
+      );
+    </script>
     <!-- //script -->
   </body>
 </html>
